cb1aa182d8925316405265dd3237f5005165fedc
suzain9084 <suzain@ce.du.ac.in>
2026-03-03 15:26:54 +0530

    add testing route for deployment

--- backend/routes/socket.py
+++ backend/routes/socket.py
@@ -24,4 +24,8 @@ async def websocket_room(websocket: WebSocket, room_id: str, name: str):
         await manager.disconnect(room_id, websocket)
         print("Unexpected error:", e)
 
-
+@router.websocket("/ws/test")
+async def test_socket(websocket: WebSocket):
+    await websocket.accept()
+    await websocket.send_text("connected")
+    await websocket.close()
